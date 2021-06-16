92287e108f840b0633d54c295a31337f77cf775f
Bradford-Meziere <coleokc@gmail.com>
2021-06-16 12:52:09 -0500

    Task 1 as a class

--- index.js
+++ index.js
@@ -39,16 +39,47 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
   }
- 
- 
 
+  Person.prototype.eat = function(edible){
+    if(this.stomach.length <= 10){
+      this.stomach.push(edible);
+    }
+  }
   
+  Person.prototype.poop = function(){
+    this.stomach = []
+  }
   
-  
-  
+  Person.prototype.toString = function(){
+    return `${this.name}, ${this.age}`;
+  }
+
+  const romy = new Person('Romy', 29);
+  const bradly = new Person('Bradly', 27)
+  const jordan = new Person('Jordan', 24)
+
+
+console.log(romy)
+console.log(bradly)
+console.log(jordan)
+
+jordan.eat('Tacos')
+jordan.eat('Sandwich')
+jordan.eat('Sushi')
+jordan.eat('Ramen')
+jordan.eat('Pizza')
+jordan.eat('Bag of Chips')
+
+
+console.log(jordan.poop())
+console.log(jordan.stomach)
+console.log(jordan.toString)
+
   /*
     TASK 2
       - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
